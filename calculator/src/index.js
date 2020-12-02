a8d65e72dfe636c2c1d02d5929cbb9f3c308e000
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2020-12-02 15:29:02 -0400

    inicio do desenvolvimento da calculadora em React.js

---
+++ calculator/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calculador from './main/Calculator'
+import './index.css';
+
+
+ReactDOM.render(
+  <div>
+    <h1>Calculadora</h1>
+    <Calculador></Calculador>
+  </div>
+  ,
+  document.getElementById('root')
+);
