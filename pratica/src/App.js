70f6a2c67bc728f120eb7734ec22dfcb27bdf98a
William David Martins de Almeida <wdma.lic17@uea.edu.br>
2020-12-23 14:45:57 -0300

    add aula sobre componentes

--- pratica/src/App.js
+++ pratica/src/App.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+class Equipe extends Component {
+    render(){
+        return(
+            <div>
+                <Sobre nome={this.props.nome} idade={this.props.idade}></Sobre>
+                <hr></hr>
+            </div>
+        );
+    }
+}
+
+class Sobre extends Component {
+    render(){
+        return(
+            <div>
+                <h2>{this.props.nome}</h2>
+                <h2>{this.props.idade}</h2>
+            </div>
+        )
+    }
+}
 export default function App(){
     return(
         <div>
-            <h1>Teste</h1>
+            <h1>Conheça nossa equipe</h1>
+            <Equipe idade={13}nome="Matheus"></Equipe>
+            <Equipe idade={56}nome="William"></Equipe>
         </div>
     )
-};
+}
